d95b545300571ee8ef8b0ab4710677d1a9888f10
Spade-9 <142297011+Spade-9@users.noreply.github.com>
2023-08-17 17:36:27 +0800

    Update 14.求最大值.c

--- 14.求最大值.c
+++ 14.求最大值.c
@@ -1,3 +1,5 @@
+////求最大值
+//
 //#define _CRT_SECURE_NO_WARNINGS 
 //
 //#include <stdio.h>
@@ -7,12 +9,19 @@
 //#include <math.h>
 //
 //
-////求最大值
+//
 //int main()
 //{
-//	int arr[] = { -1,-5,-7,8,9,-99,1000,99999,-684,783,86 };
-//	int max = arr[0];
+//	//int arr[] = { 0 };//此处没有指定数组内元素的个数，就会根据初始化的内容来推算元素的个数
+//	//即 int arr[]={0} 相当于 int arr[1]={0},后期放入内容会导致错误
+//	int arr[10] = { 0 };
 //	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//
+//	int max = arr[0];
 //	int sz = sizeof(arr) / sizeof(arr[0]);
 //	for (i = 1; i < sz; i++)
 //	{
