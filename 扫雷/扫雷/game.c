07e03834498defe4b0ba70e9e063c5f4467707fe
Spade-9 <142297011+Spade-9@users.noreply.github.com>
2023-08-22 21:56:37 +0800

    Add files via upload

---
+++ 扫雷/扫雷/game.c
@@ -0,0 +1,131 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+#include "game.h"
+
+void InitBoard(char board[ROWS][COLS], int rows, int cols, char set)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < rows; i++)
+	{
+		for (j = 0; j < cols; j++)
+		{
+			board[i][j] = set;
+		}
+	}
+
+}
+
+
+void DisplayBoard(char board[ROWS][COLS], int row, int col)
+{
+	int i = 0;
+	int j = 0;
+	printf("--------扫雷游戏--------\n");
+	//打印列号
+	for (j = 0; j <= col; j++)
+	{
+		printf("%d ", j);
+	}
+	printf("\n");
+	for (i = 1; i <= row; i++)
+	{
+		//打印行号
+		printf("%d ", i);
+		for (j = 1; j <= col; j++)
+		{
+			printf("%c ", board[i][j]);
+		}
+		printf("\n");
+	}
+	printf("-------------------------\n");
+}
+
+
+void SetMine(char board[ROWS][COLS], int row, int col)
+{
+	int count = EASY_COUNT;
+	while (count)
+	{
+		int x = rand() % row + 1;
+		int y = rand() % col + 1;
+
+		if (board[x][y] == '0')
+		{
+			board[x][y] = '1';
+			count--;
+		}
+	}
+}
+
+
+//'1'-'0'-->1
+//'0'-'0'-->0
+int get_mine_count(char board[ROWS][COLS], int x, int y)
+{
+	return (board[x - 1][y] + 
+		board[x - 1][y - 1] + 
+		board[x][y - 1] + 
+		board[x + 1][y - 1] + 
+		board[x + 1][y] + 
+		board[x + 1][y + 1] + 
+		board[x][y + 1] + 
+		board[x - 1][y + 1] - 8 * '0');
+}
+
+
+
+void FindMine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col)
+{
+	int x = 0;
+	int y = 0;
+	int win = 0;//找到非雷的个数
+
+	while (win<row*col-EASY_COUNT)
+	{
+		printf("请输入要排查的坐标:");
+		scanf("%d %d", &x, &y);
+		if (x >= 1 && x <= row && y >= 1 && y <= col)
+		{
+			if (show[x][y] != '*')
+			{
+				printf("该坐标已排查过,请重新输入");
+			}
+			else
+			{
+				//踩雷
+				if (mine[x][y] == '1')
+				{
+					printf("你被炸死了!!!\n");
+					DisplayBoard(mine, ROW, COL);
+					break;
+				}
+				//没踩雷
+				else
+				{
+					win++;
+					//统计mine数组中x,y坐标周围有几个雷
+					int count = get_mine_count(mine, x, y);
+					show[x][y] = count + '0';//转换成数字字符
+					DisplayBoard(show, ROW, COL);
+				}
+			}
+		}
+		else
+		{
+			printf("输入坐标错误,请重新输入\n");
+		}
+	}
+	if (win == row * col - EASY_COUNT)
+	{
+		printf("排雷成功!!!\n");
+		DisplayBoard(mine, ROW, COL);
+	}
+
+}
+
+
+//递归展开一片
+//1.该坐标不是雷
+//2.该坐标周围没有雷
+//3.坐标没有被排查过
